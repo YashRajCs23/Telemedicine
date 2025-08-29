b444fe748addb27f2839593797f0bc84e015e760
Yash Raj Srivastava <yashraj102723@gmail.com>
2025-08-29 14:31:25 +0530

    [yash] added user login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Login from './components/Landing/UserLogin.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className=' bg-amber-300 text-blue-500 '>hii</h1>
+      <Login />
     </>
   )
 }
